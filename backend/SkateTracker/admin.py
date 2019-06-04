67be789129d5c3434ab0abf1adabcacce44eba91
Jalani Paul <jalani2727@gmail.com>
2019-06-04 14:51:33 -0400

    added Tricks model

--- backend/SkateTracker/admin.py
+++ backend/SkateTracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tricks
+
+# Remember to import any other models you want to manage once you get through the walkthrough
 
 # Register your models here.
--- backend/SkateTracker/models.py
+++ backend/SkateTracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Tricks(models.Model):
+      title = models.CharField(max_length=120)
+      description = models.TextField()
+      completed = models.BooleanField(default=False)
+
+      def _str_(self):
+        return self.title
+
+# I'm going to want to have tricks as sub categories for the Main Categories: Ollies, Flips, Grinds, & Transition
