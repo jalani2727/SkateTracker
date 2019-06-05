69b6576a8303b613d2694365868b0e9f4c333a48
Jalani Paul <jalani2727@gmail.com>
2019-06-05 09:50:29 -0400

    create-react-app

--- backend/SkateTracker/models.py
+++ backend/SkateTracker/models.py
@@ -5,7 +5,7 @@ class Trick(models.Model):
       title = models.CharField(max_length=120)
       description = models.TextField()
       completed = models.BooleanField(default=False)
-
+    
       def _str_(self):
         return self.title
 
