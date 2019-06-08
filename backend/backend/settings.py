6f76f421930e8c35404d1b93fa84c446df435b8f
Jalani Paul <jalani2727@gmail.com>
2019-06-08 19:24:20 -0400

    requirements added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'SkateTracker',
-    'corsheaders',            # add this
+    'corsheaders',            
     'rest_framework'
 ]
 
