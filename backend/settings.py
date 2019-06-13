4a1bbb725379b3f80d29dcdd762be88310f4dbf8
Jalani Paul <jalani2727@gmail.com>
2019-06-12 20:45:06 -0400

    npm run build

--- backend/settings.py
+++ backend/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend', "build", "static"),  # update the STATICFILES_DIRS
+    os.path.join(BASE_DIR, 'frontend', "build/static"),  # update the STATICFILES_DIRS
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
