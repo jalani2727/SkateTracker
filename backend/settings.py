4866becbc3897fdec19be95929f45462d3ec8949
Jalani Paul <jalani2727@gmail.com>
2019-07-06 18:10:53 -0400

    removed APP_DIRS: True

--- backend/settings.py
+++ backend/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'build')],
-        'APP_DIRS': True,
+        # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
