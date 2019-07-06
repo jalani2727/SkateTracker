5b464776e80ac81bb901370b1b699eea3e802920
Jalani Paul <jalani2727@gmail.com>
2019-07-06 18:34:48 -0400

    moved build to SkateTracker folder

---
+++ SkateTracker/build/precache-manifest.6dec513aaa736ee21688e5ccb17053d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52926e3e4e6a5bbc4336f52659846893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3324f347ad0c89c5129c",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5f4c617db4a1e921f196",
+    "url": "/static/css/main.68818f64.chunk.css"
+  },
+  {
+    "revision": "3324f347ad0c89c5129c",
+    "url": "/static/js/2.a9d303c6.chunk.js"
+  },
+  {
+    "revision": "5f4c617db4a1e921f196",
+    "url": "/static/js/main.eec1694c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
