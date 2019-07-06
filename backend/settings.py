31b31822a1cc50ec7546036d8dfafa188f88ec9d
Jalani Paul <jalani2727@gmail.com>
2019-07-06 18:29:57 -0400

    .

--- backend/settings.py
+++ backend/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'corsheaders' 
+    'corsheaders', 
 
-    'SkateTracker',
+    'SkateTracker'
 ]
 
 MIDDLEWARE = [
