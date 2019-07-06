2bc388f3c9b81820a99468d518a35c9813ff2eb9
Jalani Paul <jalani2727@gmail.com>
2019-07-06 18:50:56 -0400

    cookie experiement

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 
 axios.defaults.withCredentials = true;
-// axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfCookieName = 'csrftoken'
 // axios.defaults.xsrfHeaderName = "X-CSRFToken"
 
   
