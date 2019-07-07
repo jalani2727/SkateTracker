70abd7bc1aebf6f4bc9c1bc37c77aeca112b1543
Jalani Paul <jalani2727@gmail.com>
2019-07-07 09:19:30 -0400

    .

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's3ccl8nq(_n_h!dqenb3^36b-cib#(box1+%z*=x-dusut!4f5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['skatetracker.herokuapp.com', '127.0.0.1:8000', '127.0.0.1', 'localhost']
 
